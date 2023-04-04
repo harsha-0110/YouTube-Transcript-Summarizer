5954023848d82b8ac4c009452ff08925066b363a
harsha-0110 <112392323+harsha-0110@users.noreply.github.com>
2023-04-04 10:29:15 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -34,51 +34,47 @@ def summarize(text):
   sumValues = 0
   for sentence in sentenceValue:
     sumValues += sentenceValue[sentence]
-
   average = int(sumValues / len(sentenceValue))
-
   summary = ''
   for sentence in sentences:
     if (sentence in sentenceValue) and (sentenceValue[sentence] > (1.2 * average)):
       summary += " " + sentence
-    
   return summary
 
 def summarize_text(text, chunk_size=500, max_length=100, min_length=10, num_beams=4, gpu_device=0):
-    # Load the tokenizer and model
-    tokenizer = AutoTokenizer.from_pretrained("sshleifer/distilbart-xsum-12-6")
-    model = AutoModelForSeq2SeqLM.from_pretrained("sshleifer/distilbart-xsum-12-6").to(gpu_device)
+  # Load the tokenizer and model
+  tokenizer = AutoTokenizer.from_pretrained("sshleifer/distilbart-xsum-12-6")
+  model = AutoModelForSeq2SeqLM.from_pretrained("sshleifer/distilbart-xsum-12-6").to(gpu_device)
 
-    # Split the input text into chunks
-    chunks = [text[i:i+chunk_size] for i in range(0, len(text), chunk_size)]
+  # Split the input text into chunks
+  chunks = [text[i:i+chunk_size] for i in range(0, len(text), chunk_size)]
 
-    # Summarize each chunk and combine the summaries
-    summaries = []
-    for chunk in chunks:
-        inputs = tokenizer.encode(chunk, return_tensors='pt').to(gpu_device)
-        summary_ids = model.generate(
-            inputs,
-            max_length=max_length,
-            min_length=min_length,
-            num_beams=num_beams,
-            early_stopping=True
-        )
-        summary = tokenizer.decode(summary_ids.squeeze(), skip_special_tokens=True)
-        summaries.append(summary)
-    summary = ' '.join(summaries)
-
-    return summary
+  # Summarize each chunk and combine the summaries
+  summaries = []
+  for chunk in chunks:
+      inputs = tokenizer.encode(chunk, return_tensors='pt').to(gpu_device)
+      summary_ids = model.generate(
+          inputs,
+          max_length=max_length,
+          min_length=min_length,
+          num_beams=num_beams,
+          early_stopping=True
+      )
+      summary = tokenizer.decode(summary_ids.squeeze(), skip_special_tokens=True)
+      summaries.append(summary)
+  summary = ' '.join(summaries)
+  return summary
 
 def get_transcript(video_link):
-    video_id = re.search(r'v=([^&]*)', video_link).group(1)
-    transcript = yta.get_transcript(video_id)
-    lines = ""
-    temp = ""
-    for line in transcript:
-      temp = line['text']
-      lines += temp.replace("\n", " ")
-    with open("clean.txt", "w") as f:
-      f.write(lines)
+  video_id = re.search(r'v=([^&]*)', video_link).group(1)
+  transcript = yta.get_transcript(video_id)
+  lines = ""
+  temp = ""
+  for line in transcript:
+    temp = line['text']
+    lines += temp.replace("\n", " ")
+  with open("clean.txt", "w") as f:
+    f.write(lines)
 
 app = Flask(__name__)
 
